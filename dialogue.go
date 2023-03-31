92c0a22ee9065b6632795bff8f1f5e87d315a13a
Dj_Mike238 <22237379+DjMike238@users.noreply.github.com>
2023-03-31 13:51:06 +0200

    Move struct types in separate file

--- dialogue.go
+++ dialogue.go
@@ -3,54 +3,9 @@ package main
 import (
 	"encoding/json"
 	"os"
-	"time"
 
 	"github.com/gen2brain/raylib-go/raylib"
 )
-
-// Hypotetical moods for characters
-type Mood string
-
-const (
-	Normal Mood = "normal"
-	Idle        = "idle"
-)
-
-// Name box position
-type NamePos string
-
-const (
-	Hidden NamePos = "hidden"
-	Left            = "left"
-	Center          = "center"
-	Right           = "right"
-)
-
-// Blip tone to play when printing text
-type Tone string
-
-const (
-	Silent Tone = "silent"
-	Blip        = "blip"
-	Med         = "med"
-	High        = "high"
-)
-
-// Data type containing dialogue line data
-type DialogueLine struct {
-	NamePos NamePos      `json:"name_position,omitempty"`
-	Mood     Mood          `json:"mood,omitempty"`
-	Name     string        `json:"character,omitempty"`
-	Text     string        `json:"text,omitempty"`
-	Pause    time.Duration `json:"pause,omitempty"`
-	Autoplay bool          `json:"autoplay,omitempty"`
-}
-
-// Data type containing character data
-type Character struct {
-	Name string `json:"name"`
-	Tone Tone   `json:"tone"`
-}
 
 var (
 	characters []Character
---
+++ types.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"time"
+)
+
+// Hypotetical moods for characters
+type Mood string
+
+const (
+	Normal Mood = "normal"
+	Idle        = "idle"
+)
+
+// Name box position
+type NamePos string
+
+const (
+	Hidden NamePos = "hidden"
+	Left            = "left"
+	Center          = "center"
+	Right           = "right"
+)
+
+// Blip tone to play when printing text
+type Tone string
+
+const (
+	Silent Tone = "silent"
+	Blip        = "blip"
+	Med         = "med"
+	High        = "high"
+)
+
+// Data type containing dialogue line data
+type DialogueLine struct {
+	NamePos NamePos      `json:"name_position,omitempty"`
+	Mood     Mood          `json:"mood,omitempty"`
+	Name     string        `json:"character,omitempty"`
+	Text     string        `json:"text,omitempty"`
+	Pause    time.Duration `json:"pause,omitempty"`
+	Autoplay bool          `json:"autoplay,omitempty"`
+}
+
+// Data type containing character data
+type Character struct {
+	Name string `json:"name"`
+	Tone Tone   `json:"tone"`
+}
